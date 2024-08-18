3422c00a19a8fb69c0e4175ee102ae945771fb7b
redacted <125401979+captainzero93@users.noreply.github.com>
2024-08-18 20:27:55 +0100

    Add files via upload

---
+++ install.py
@@ -0,0 +1,12 @@
+import launch
+
+if not launch.is_installed("safetensors"):
+    launch.run_pip("install safetensors", "requirements for UNet Loader")
+
+if not launch.is_installed("torch"):
+    launch.run_pip("install torch", "requirements for UNet Loader")
+
+if not launch.is_installed("asyncio"):
+    launch.run_pip("install asyncio", "requirements for UNet Loader")
+
+print("UNet Loader extension installed successfully.")

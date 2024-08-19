58b1a0ffb98f44c5d3cb03cce000257731f91cd2
redacted <125401979+captainzero93@users.noreply.github.com>
2024-08-19 19:13:04 +0100

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,10 @@
-from .unet_loader import register_unet_loader
+from .unet_loader import on_app_started, on_ui_tabs
+from modules import script_callbacks
 
-def on_app_started(demo, app):
-    register_unet_loader()
+print("UNet Loader __init__.py is being executed")
+
+def initialize():
+    script_callbacks.on_ui_tabs(on_ui_tabs)
+    script_callbacks.on_app_started(on_app_started)
+
+print("UNet Loader __init__.py execution completed")
